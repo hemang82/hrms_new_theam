a6beaa2fc5950dbedec7bd0975853364b043e56b
hemang82 <109712867+hemang82@users.noreply.github.com>
2025-09-09 12:28:39 +0530

    Change cod

--- src/pages/auth/Login/index.js
+++ src/pages/auth/Login/index.js
@@ -61,7 +61,7 @@ const Login = () => {
                 <div className="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
                     <div className="d-flex align-items-center justify-content-center w-100">
                         <div className="row justify-content-center w-100">
-                            <div className="col-md-8 col-lg-6 col-xxl-5">
+                            <div className="col-md-8 col-lg-6 col-xxl-4">
                                 <div className="card mb-0">
                                     <div className="card-body">
                                         <a className="text-nowrap logo-img text-center d-block mb-3 w-100">
